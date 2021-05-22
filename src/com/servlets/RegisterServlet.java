e04c9b85f6fc926c3ff75f38406bc88a8036d267
tanaya123 <tanayawankar58@gmail.com>
2021-05-22 10:23:46 +0530

    Started with frontend

---
+++ src/com/servlets/RegisterServlet.java
@@ -0,0 +1,14 @@
+package com.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet
+{
+	public void service(HttpServletRequest req,HttpServletResponse res)
+	{
+		
+	}
+
+}
